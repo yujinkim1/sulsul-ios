3814480a640f8331deafe81a3a4f10fdea00891a
jun12 <jun12@utransfer.com>
2023-12-05 18:50:41 +0900

    fix: Almofire, Then 프레임워크 Static -> Dynamic으로 수정

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -16,6 +16,10 @@ let dependencies = Dependencies(
             .snapKit,
             .then,
             .swinject
+        ],
+        productTypes: [
+            "Alamofire": .framework,
+            "Then": .framework
         ]
     ),
     platforms: [.iOS]
@@ -23,7 +27,7 @@ let dependencies = Dependencies(
 
 public extension Package {
     static let alamofire: Package = .remote(url: "https://github.com/Alamofire/Alamofire",
-                                            requirement: .branch("master"))
+                                            requirement: .upToNextMajor(from: "5.8.1"))
     static let snapKit: Package = .remote(url: "https://github.com/SnapKit/SnapKit.git",
                                           requirement: .upToNextMajor(from: "5.0.1"))
     static let then: Package = .remote(url: "https://github.com/devxoul/Then",
