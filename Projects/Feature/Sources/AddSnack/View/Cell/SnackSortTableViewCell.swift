07ca2a9d5af5638c9c4bd77c9bdb19806971a36c
Y00ujin <71479613+Y00ujin@users.noreply.github.com>
2024-01-04 08:21:45 +0900

    feat: 안주 선택 시 체크 표시 노출

--- Projects/Feature/Sources/AddSnack/View/Cell/SnackSortTableViewCell.swift
+++ Projects/Feature/Sources/AddSnack/View/Cell/SnackSortTableViewCell.swift
@@ -15,6 +15,10 @@ final class SnackSortTableViewCell: UITableViewCell {
         $0.layer.cornerRadius = moderateScale(number: 8)
     }
     
+    private lazy var checkImageView = UIImageView().then {
+        $0.image = UIImage(named: "common_snackCheck")
+    }
+    
     private lazy var snackSortLabel = UILabel().then {
         $0.font = Font.medium(size: 16)
         $0.textColor = DesignSystemAsset.gray900.color
@@ -34,6 +38,7 @@ final class SnackSortTableViewCell: UITableViewCell {
     func bind(_ snackSortModel: SnackSortModel) {
         snackSortLabel.text = snackSortModel.name
         cellBackButton.backgroundColor = snackSortModel.isSelect ? DesignSystemAsset.gray200.color : DesignSystemAsset.gray100.color
+        checkImageView.isHidden = !snackSortModel.isSelect
     }
 }
 
@@ -41,6 +46,7 @@ extension SnackSortTableViewCell {
     private func layout() {
         contentView.addSubview(cellBackButton)
         contentView.addSubview(snackSortLabel)
+        contentView.addSubview(checkImageView)
         
         cellBackButton.snp.makeConstraints {
             $0.top.leading.trailing.equalToSuperview()
@@ -50,6 +56,12 @@ extension SnackSortTableViewCell {
         snackSortLabel.snp.makeConstraints {
             $0.leading.equalToSuperview().inset(moderateScale(number: 12))
             $0.centerY.equalToSuperview()
+        }
+        
+        checkImageView.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(moderateScale(number: 16))
+            $0.centerY.equalToSuperview()
+            $0.size.equalTo(moderateScale(number: 24))
         }
     }
 }
