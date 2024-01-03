feb110018357d123f0666bbaff638bf6fd5bdfeb
Y00ujin <71479613+Y00ujin@users.noreply.github.com>
2024-01-03 22:29:55 +0900

    feat: 안주 이름 글자수 제한 로직 추가

--- Projects/Feature/Sources/AddSnack/View/AddSnackViewController.swift
+++ Projects/Feature/Sources/AddSnack/View/AddSnackViewController.swift
@@ -258,6 +258,16 @@ extension AddSnackViewController: UITextFieldDelegate {
             submitButton.backgroundColor = UIColor.yellow
         }
     }
+    
+    public func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let textFieldText = textField.text,
+            let rangeOfTextToReplace = Range(range, in: textFieldText) else {
+                return false
+        }
+        let substringToReplace = textFieldText[rangeOfTextToReplace]
+        let count = textFieldText.count - substringToReplace.count + string.count
+        return count <= 30
+    }
 }
 
 extension AddSnackViewController: UIScrollViewDelegate {
