d5f338a5c52327b6aef069ca5a6d8ae74094e996
Y00ujin <71479613+Y00ujin@users.noreply.github.com>
2024-02-15 08:14:32 +0900

    refactor: 그림자, 그라디언트 설정 함수 생성, 새 피드 작성 그림자 뷰 추가

--- Projects/Feature/Sources/WriteFeed/SelectPhoto/View/SelectPhotoViewController.swift
+++ Projects/Feature/Sources/WriteFeed/SelectPhoto/View/SelectPhotoViewController.swift
@@ -48,9 +48,7 @@ public class SelectPhotoViewController: BaseViewController {
         $0.text = "0/5"
     }
     
-    private lazy var bottomShadowView = UIView().then {
-        $0.backgroundColor = DesignSystemAsset.black.color
-    }
+    private lazy var bottomShadowView = UIView()
     
     private lazy var flowLayout = UICollectionViewFlowLayout().then {
         $0.minimumLineSpacing = 5.67
@@ -84,6 +82,12 @@ public class SelectPhotoViewController: BaseViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func viewWillLayoutSubviews() {
+        bottomShadowView.setGradient(startColor: DesignSystemAsset.black.color.withAlphaComponent(0.0),
+                                     endColor: DesignSystemAsset.black.color,
+                                     location: [0, 0.8])
     }
     
     public override func viewDidLoad() {
