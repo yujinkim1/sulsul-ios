236eccbe09802c11bf212f33bc723e734165fe16
yujinkim1 <yujin7621@me.com>
2024-06-07 23:43:27 +0900

    EnvironmentPlugin 적용

--- Tuist/Config.swift
+++ Tuist/Config.swift
@@ -3,5 +3,8 @@ import ProjectDescription
 let config = Config(
     compatibleXcodeVersions: .all,
     swiftVersion: "5.0",
+    plugins: [
+        .local(path: .relativeToManifest("../../Plugins/EnvironmentPlugin")),
+    ],
     generationOptions: .options()
 )
